75c2ae06fa210f320d9fccc3bfdb1845ddc44309
Vaibhav Biradar <vaibhavbb007@gmail.com>
2026-09-05 16:40:13 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        if(nums.length == 0) {
+            return -1;
+        }
+        if(nums.length == 1){
+            if(nums[0] == target){
+                return 0;
+            } else{
+                return -1;
+            }
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right){
+            int mid = (left + right)/2;
+            if(target > nums[mid]){
+                left = mid + 1;
+            } else if (target < nums[mid]){
+                right = mid - 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
